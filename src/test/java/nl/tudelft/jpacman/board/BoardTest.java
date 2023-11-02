f31ba88ef1c60cc72c673a41a65d6eeced4644ab
Bunradar Chatchaiyadech <82015439+BunradarCH@users.noreply.github.com>
2023-11-02 14:18:30 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,36 +4,65 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
-
+    /***
+     *
+     */
     private Board board;
 
+    /***
+     *
+     */
     @BeforeEach
     void setUp(){
         Square[][] grid = new Square[1][1];
         grid[0][0] = new BasicSquare();
         board = new Board(grid);
     }
+
+    /***
+     *
+     */
     @Test
     void testGetWidth() {
         assertThat(board.getWidth()).isEqualTo(1);
     }
+
+    /***
+     *
+     */
     @Test
     void testGetHeight() {
         assertThat(board.getHeight()).isEqualTo(1);
     }
+
+    /***
+     *
+     */
     @Test
     void testBasicSquare() {
         assertThat(board).isNotNull();
     }
+
+    /***
+     *
+     */
     @Test
     void testInvariant() {
         assertThat(board.invariant()).isTrue();
     }
+
+    /***
+     *
+     */
     @Test
     void testSquareAt() {
         Square square = board.squareAt(0, 0);
         assertThat(square).isInstanceOf(BasicSquare.class);
     }
+
+    /***
+     *
+     */
     @Test
     void testSquareAtNull() {
         Square[][] grid = new Square[1][1];
